5afbfda4336e66fb9ef68114a59b59160054bbcf
Igor Belov <belov78@gmail.com>
2020-01-06 14:08:00 +0200

    add fake mail type

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from activity import *
 ldr = Loader(KeyChain.LOADER_KEY)
 ldr.register(Email)
 ldr.register(LoaderStateReporter)
-ldr.register(FakeEmail)
+# ldr.register(FakeEmail)
 ldr.track_schedule()
