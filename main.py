a8b061f0eee366032dc49e7f7aa827f4a83bb8db
Igor Belov <belov78@gmail.com>
2020-01-09 21:59:17 +0300

    * deploy IS Sync to production

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from activity import *
 
 ldr = Loader(KeyChain.LOADER_KEY)
+ldr.get_PG_connector = lambda: PGConnector(KeyChain.PG_KEY)
+ldr.get_IS_connector = lambda: ISConnector(KeyChain.IS_KEY)
 ldr.register(Email)
 ldr.register(LoaderStateReporter)
-# ldr.register(FakeEmail)
+ldr.register(ISActualizer)
+ldr.register(ISSync)
 ldr.track_schedule()
