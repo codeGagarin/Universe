df7cfab4a41b768ba16375c9a755fff8e3c9f2f1
Igor Belov <belov78@gmail.com>
2020-09-25 00:10:00 +0300

    * label print upgrade

--- activities/techjrnl.py
+++ activities/techjrnl.py
@@ -11,9 +11,16 @@ class TJSync(Activity):
         ftp_con = utils.connect_server(ftp_key)
         log_files = utils.get_tj_files_for_sync(ftp_con, 50)
         adapter = utils.PGAdapter(KeyChain.PG_PERF_KEY, ftp_key['user'])
+        files_ok = 0
+        files_fail = 0
         for file in log_files:
             is_ok = utils.parse_log_file(ftp_con, file, adapter)
-        print(f"{ftp_key['user']}: {'Ok' if is_ok else 'Fail'}")
+            if is_ok:
+                files_ok += 1
+            else:
+                files_fail += 1
+
+        print(f"{ftp_key['user']}: [TJ] f:{files_fail}.k:{files_ok}")
         ftp_con.close()
 
 
