52c053c9ccb09c5598a405e9413049f242570b28
Igor Belov <belov78@gmail.com>
2020-10-08 23:20:25 +0300

    + register level scan activity

--- activities/reg.py
+++ activities/reg.py
@@ -1,7 +1,7 @@
 from activities.activity import Email
 from activities.intraservice import ISActualizer, ISSync, IS404TaskCloser
 from activities.reports import LoaderStateReporter2, HelpdeskWeekly
-from activities.techjrnl import TJSync
+from activities.techjrnl import TJSync, LevelScan
 
 
 def init_ldr(ldr):
@@ -12,6 +12,8 @@ def init_ldr(ldr):
     ldr.register(HelpdeskWeekly)
     ldr.register(IS404TaskCloser)
     ldr.register(TJSync)
+    ldr.register(LevelScan)
+
 
 
 
