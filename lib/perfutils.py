fac0a24b3ae68a0884970e91468de106cb4c13c2
Igor Belov <belov78@gmail.com>
2020-11-18 22:43:10 +0300

    * DutyActivity turn-on

--- lib/perfutils.py
+++ lib/perfutils.py
@@ -603,7 +603,7 @@ class PGAdapter(ABaseAdapter):
 
 class DutyActivity(Activity):
     def get_crontab(self):
-        return '' # '*/5 * * * *'
+        return '*/5 * * * *'
 
     def run(self):
         db_key = KeyChain.PG_PERF_KEY
