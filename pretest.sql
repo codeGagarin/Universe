eb3b10446dd04eaea3e501f995146e6224a9c7f0
Igor Belov <belov78@gmail.com>
2021-03-14 18:49:53 +0300

    Data restore improve

--- pretest.sql
+++ pretest.sql
@@ -1,1 +1,5 @@
-delete from "Loader" WHERE "status"='todo'
+with
+    pids as (select pid from pg_stat_activity where datname='mirror_test' and pid!=pg_backend_pid())
+select pid, pg_terminate_backend(pid) from pids
+
+--delete from "Loader" WHERE "status"='todo'
