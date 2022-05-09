426653c033626913410f8960ba8f041bff441b6f
igor <belov78@gmail.com>
2022-05-09 15:27:58 +0300

    + Starter telegram alarmer

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from lib.pg_starter import PGStarter
 from keys import KeyChain
 from lib.reports.report_reg import report_list
 from lib.activity_reg import activity_list
+from lib.telebots.starter_alarm import TelegramAlarmer
 
 
 def make_starter():
-    return PGStarter(activity_list=activity_list,  report_list=report_list)
+    return PGStarter(activity_list=activity_list,  report_list=report_list, alarmer=TelegramAlarmer())
 
 
 make_starter().track_schedule()
