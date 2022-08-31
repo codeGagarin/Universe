b80bfa5eca5c740925ff1010cbe89fb6292a81b8
igor <belov78@gmail.com>
2022-08-31 12:54:35 +0300

    * fix bug

--- prc/dns-sync/sync.py
+++ prc/dns-sync/sync.py
@@ -109,7 +109,7 @@ def _host_sorter(v):
 
 def main():
     info('Request external records.')
-    ext_records, report['external_zone'] = reg_api.get_external_a_records(sorter=_host_sorter)
+    ext_records = report['external_zone'] = reg_api.get_external_a_records(sorter=_host_sorter)
 
     info('Request internal records:')
     int_records = report['zone_before'] = win_api.get_internal_a_records(sorter=_host_sorter)
